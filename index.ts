6d5b97d7acaefbd68e6480e69b696e8d41ade8e8
HyperSniper89 <jakup.dam20@student.setur.fo>
2022-02-05 12:34:40 +0000

    Installera left-pad. Adda index.ts við mini tutorial.

---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * Skriva sum JS ikki sum TS, tí reyð strika undir require
+ * TS er eitt superset av JS, og kann koyra alla JS kotu
+ */
+
+const leftPad = require('left-pad') // Require left pad
+const output = leftPad('Hello, World!', 15) // Define output
+
+// Send output to the console
+console.log(output)  // small tutorial í console "node index.ts"
