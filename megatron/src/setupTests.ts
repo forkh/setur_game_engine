fd7671a119ad4f07bf5df1a8059a962925d12d8b
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-02-06 10:26:52 +0000

    Added skeletal test for renderer method.

--- megatron/src/setupTests.ts
+++ megatron/src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import {renderer} from './Renderer';
+
+describe("Renderer test", () => {
+  test("should try to render sprites to canvas.", async () => {
+    // ???
+  });
+});
