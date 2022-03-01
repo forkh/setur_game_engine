ecaee1803f99af4f652ead4b58743f6598ddd86b
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-01 14:31:29 +0000

    Small vector class

---
+++ megatron/src/engine/math.ts
@@ -0,0 +1,28 @@
+class Vector2d {
+    private x: number;
+    private y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public add(v: Vector2d): void {
+        this.x += v.x;
+        this.y += v.y;
+    }
+
+    public subtraction(v: Vector2d): void {
+        this.x -= v.x;
+        this.y -= v.y;
+    }
+
+    public get(): Vector2d {
+        return this;
+    }
+
+    // Add dotproducts and more ?
+}
+
+
+export {Vector2d};
