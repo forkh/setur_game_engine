0d3b390fd162901dd72b2f102eef779498106b00
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-01 14:15:53 +0000

    Delete stuff

--- megatron/src/Constants.tsx
+++
@@ -1,3 +0,0 @@
-export class Constants {
-    public oid: number = 1;
-}
--- megatron/src/Maths.tsx
+++
@@ -1,8 +0,0 @@
-// Bara ein funktión so okkurt er her.
-// Allar math funktionir vit brúka, sum ikki eru í JS skulu inn á her.
-export function pow(n: number, p: number) : number{
-    let res: number;
-    res = n;
-    while (1 < p) res *= n;
-    return res;
-}
