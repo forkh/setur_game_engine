4d7dce7a883f0331572cde30550ccf11a78e0f16
Rúni Klein Hansen <runi.han20@student.setur.fo>
2022-03-04 22:21:28 +0000

    CSS Renderer test

--- megatron/src/engine/CSSRenderer.tsx
+++ megatron/src/engine/CSSRenderer.tsx
@@ -1,4 +1,10 @@
+import {ReactNode} from "react";
+import {render} from "react-dom";
 
+function renderer(id: string): ReactNode {
+    return <div id={id}>
 
+    </div>
+}
 
-export {};
+export {renderer};
