1e72e219ae773a8fb30a1ddf70c829cb09927943
Torkil <torkil.tho20@student.setur.fo>
2022-03-05 15:11:02 +0000

    test

--- megatron/src/App.tsx
+++ megatron/src/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Canvas from "./engine/Canvas";
-import {Physics} from "./engine/Physics/Physics";
 
 function App() {
-
     const draw = (ctx: any, frameCount: number) => {
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
         ctx.fillStyle = '#000000'
         ctx.beginPath()
-        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+        ctx.arc(100, 100, 100 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
         ctx.fill()
+
     }
+
 
     return <Canvas draw={draw}/>
 }
+
+
 
 
 export default App;
