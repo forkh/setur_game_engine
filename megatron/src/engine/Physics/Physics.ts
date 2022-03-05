9eb707ee3e1e38342fa146dbe4645a57ea3ea6f2
Torkil <torkil.tho20@student.setur.fo>
2022-03-05 13:03:48 +0000

    minor changes to be discussed (position)

--- megatron/src/engine/Physics/Physics.ts
+++ megatron/src/engine/Physics/Physics.ts
@@ -9,7 +9,7 @@ class Physics { // rigidbody
     public restitution: number; // fylgi guide sindur blindt at byrja við
     // (verður brúkt til inelasticCollision sama stað í guide'ini.)
     public velocity: Vector2d = new Vector2d(0,0);
-    public position: Vector2d = new Vector2d(0,0);
+    public position: Vector2d;
     public acceleration: Vector2d = new Vector2d(0,0);
 
     public constructor(position: Vector2d, restituion: number, mass = 1){
