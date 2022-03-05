5e47b2ddeb33fd6a66bacb8a72a10b2fb6a3bac8
Torkil <torkil.tho20@student.setur.fo>
2022-03-05 14:19:02 +0000

    canvas and canvashook

--- megatron/src/App.tsx
+++ megatron/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Canvas from "./engine/Canvas";
+import {Physics} from "./engine/Physics/Physics";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const draw = (ctx: any, frameCount: number) => {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+
+    return <Canvas draw={draw}/>
 }
+
 
 export default App;
---
+++ megatron/src/engine/CanvasHook.jsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from 'react'
+// https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
+
+const useCanvas = draw => {
+
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        let frameCount = 0;
+        let animationFrameId;
+
+        const render = () => {
+            frameCount++;
+            draw(context, frameCount);
+            animationFrameId = window.requestAnimationFrame(render);
+        }
+        render();
+
+        return () => {
+            window.cancelAnimationFrame(animationFrameId);
+        }
+    }, [draw])
+
+    return canvasRef;
+}
+
+export default useCanvas
