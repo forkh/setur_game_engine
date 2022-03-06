37c0b49d086802bb5eb255ad01c49e8515c68cc4
Rúni Klein Hansen <runi.han20@student.setur.fo>
2022-03-06 14:08:48 +0000

    not really working

--- megatron/src/engine/Canvas2d.tsx
+++ megatron/src/engine/Canvas2d.tsx
@@ -11,28 +11,11 @@ class Canvas2d {
         //Canvas2d.canvas = document.getElementById(canvasid) as HTMLCanvasElement;
         //Canvas2d.canvas = new HTMLCanvasElement();
         //Canvas2d.context = Canvas2d.canvas.getContext("2d");
-        this.testmethod();
+        //this.testmethod();
+        Canvas2d.canvas = document.createElement("canvas");
+        Canvas2d.context = Canvas2d.canvas.getContext("2d");
     };
     
-    public testmethod() {
-        var c: HTMLCanvasElement = document.getElementById("engine_canvas") as HTMLCanvasElement;
-        var ctx = c.getContext("2d");
-
-        return (
-          <div>
-            <h1>HTML5 Canvas + React.js</h1>
-            <canvas
-              id="myCanvas"
-              width="200"
-              height="100"
-              style={{ border: "1px solid #d3d3d3" }}
-            >
-              Your browser does not support the HTML canvas tag.
-            </canvas>
-          </div>
-        );
-    }
-
     public static instantiateCanvas2d(canvasid: string): void {
         if (!Canvas2d.instance) {
             Canvas2d.instance = new Canvas2d(canvasid);
