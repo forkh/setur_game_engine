668c7a0b1331dda7efe3c39932f63accc3b0d4d4
Torkil <torkil.tho20@student.setur.fo>
2022-03-07 14:53:38 +0000

    testing

---
+++ megatron/src/engine/Canvas4.tsx
@@ -0,0 +1,71 @@
+//import "./canvas.css";
+import React, {useRef, useEffect, useCallback, useState} from "react";
+import logo192 from "../logo192.png";
+import logo512 from "../logo512.png"
+import {
+    InputSystem,
+    EventType
+} from "./Input"
+import {InputMap} from "./Input";
+import {getKeyEventProps} from "@testing-library/user-event/dist/keyboard/getEventProps";
+import {Simulate} from "react-dom/test-utils";
+
+
+
+//import frameRenderer from "./frameRenderer";
+
+
+function Canvas() {
+    let canvasRef = useRef<HTMLCanvasElement | null>(null);
+    let requestIdRef = useRef<number | null>(null);
+
+    const size = { width: window.innerWidth, height: window.innerHeight};
+
+
+
+    const updateImage = () => {
+
+
+    };
+
+    const renderFrame = () => {
+        if (canvasRef.current) {
+            const ctx = canvasRef.current.getContext("2d");
+            ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+
+
+            updateImage();
+            //frameRenderer.call(ctx, size, ballRef.current);
+        }
+    };
+
+    const tick = () => {
+        if (!canvasRef.current) return;
+        renderFrame();
+        if (requestIdRef.current) {
+            requestIdRef.current = requestAnimationFrame(tick);
+        }
+    };
+
+    useEffect(() => {
+        requestIdRef.current = requestAnimationFrame(tick);
+        return () => {
+            if (requestIdRef.current) {
+                cancelAnimationFrame(requestIdRef.current);
+            }
+        };
+    }, []);
+
+    return <canvas {...size} ref={canvasRef} />;
+
+
+
+
+}
+
+
+
+
+
+
+export default Canvas;
