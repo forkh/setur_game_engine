830238402bbd16a68f19218fbff67ee6ac5dddc7
HyperSniper89 <jakup.dam20@student.setur.fo>
2022-03-08 19:09:52 +0000

    Raw Collision Class

--- megatron/src/engine/CollissionSystem.ts
+++ megatron/src/engine/CollissionSystem.ts
@@ -1,12 +1,21 @@
+import { System, Polygon, Circle } from "detect-collisions";
 import {Vector2d} from "./math";
 
+interface ICollider{
+    title : string;
+    x : number;
+    y : number;
+}
 
 class CollisionSystem {
+    collisionArray = [];
+    collisionSystem = new System();
+
 
     private static instance: CollisionSystem;
 
-
     private constructor() {}
+
 
     public static getInstance(): CollisionSystem {
         if (!CollisionSystem.instance) {
@@ -16,10 +25,6 @@ class CollisionSystem {
         return CollisionSystem.instance;
     }
 
-    /*public boxCollider(){
 
-        return this.boxCollider();
-
-    }*/
 
 }
