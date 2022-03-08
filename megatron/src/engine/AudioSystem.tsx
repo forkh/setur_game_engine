b9768feaac644ae09af4df62ea606aea37e93c71
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-08 23:41:46 +0000

    Added functionality for audiosystem to react to events.

--- megatron/src/engine/AudioSystem.tsx
+++ megatron/src/engine/AudioSystem.tsx
@@ -1,22 +1,68 @@
 import { ResourceManager } from './ResourceManager';
 import { ReactNode } from 'react';
 
+type EventSounds = {
+    [event: string]: number;
+}
+
+const event = new Event('audio');
+const cev = new CustomEvent('audio1', {
+    bubbles: true,
+    detail: { channel: "1" }
+});
+
 class AudioSystem {
     private static instance: AudioSystem;
-    public static channels: HTMLAudioElement[];
+    private static channels: HTMLAudioElement[];
+    private static current_channel: number;
+    private static events: EventSounds;
 
-    private constructor() {
+    private constructor(events: EventSounds) {
         AudioSystem.channels = [];
+        AudioSystem.events = {};
+
+        for (let eventsKey in events) {
+            document.addEventListener(eventsKey+events[eventsKey], () => {
+                this.playTrack(events[eventsKey]);
+            });
+        }
+
+        if (events !== {}) {
+            for (let eventsKey in events) {
+                AudioSystem.events[eventsKey] = events[eventsKey];
+            }
+        }
     }
 
-    public static instantiateAudioSystem(): void {
+    public static addTrack(sound: HTMLAudioElement, channel: number): void {
+        AudioSystem.channels[channel] = sound;
+    }
+
+    private playTrack(channel: number): void {
+        AudioSystem.channels[channel].play();
+        console.log("PLaying something??");
+    }
+
+    public static instantiateAudioSystem(events: EventSounds): void {
         if (!AudioSystem.instance) {
-            AudioSystem.instance = new AudioSystem();
+            AudioSystem.instance = new AudioSystem(events);
         }
     }
 
     public static play2() {
         AudioSystem.channels[0].play();
+    }
+
+    public static addSoundChannel(sound: HTMLAudioElement, channel: number): ReactNode {
+        AudioSystem.channels[channel] = sound;
+
+        const soundChannel: ReactNode = <audio id={String(channel)}>
+            <source src={"AudioSystem.channels[channel].src"} type={"audio/wav"}/>
+        </audio>;
+
+        document.append('<source src={"AudioSystem.channels[channel].src"} type={"audio/wav"}/>');
+
+        return soundChannel;
     }
 
     public static play(soundid: string): ReactNode {
@@ -25,7 +71,7 @@ class AudioSystem {
         return (
             <div id={"0"}>
                 <button onClick={this.play2} >Play</button>
-                <audio autoPlay loop>
+                <audio>
                     <source src={this.channels[0].src} type={"audio/wav"}></source>
                 </audio>
             </div>
@@ -34,3 +80,4 @@ class AudioSystem {
 }
 
 export {AudioSystem};
+export type {EventSounds};
