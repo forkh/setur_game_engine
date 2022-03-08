c4ba97c5c1f608f4fc9e61cbba8750a98e8fbd91
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-08 22:32:15 +0000

    InputSystem

--- megatron/src/engine/Input.ts
+++ megatron/src/engine/Input.ts
@@ -1,87 +1,42 @@
-// Inspired heavily by:
-// https://github.com/suuunly/react-sheep-engine-2
-
-type InputMap = {
-    [key: string]: string | number;
-}
+/*
+ * Based partially upon example:
+ * https://github.com/suuunly/react-sheep-engine-2/blob/main/src/react-sheep-engine/input/InputSystem.ts
+ */
 
 type InputTriggerMap = {
-    [code: string]: (() => void)[];
-}
-
-enum EventType {
-    ButtonDown,
-    ButtonUp
+    [code: string]: (() => void);
 }
 
 class InputSystem {
-    // Singleton instance variable
     private static instance: InputSystem;
 
-    private buttonDownTriggers: InputTriggerMap;
-    private buttonUpTriggers: InputTriggerMap;
+    private readonly triggers: InputTriggerMap;
 
-    private constructor(private readonly maps: InputMap) {
-        document.addEventListener("keydown", this.onButtonDown.bind(this));
-        document.addEventListener("keydown", this.onButtonUp.bind(this));
-        this.buttonDownTriggers = {};
-        this.buttonUpTriggers = {};
-    }
+    private constructor(map: InputTriggerMap) {
+        document.addEventListener("keydown", this.buttonPressed.bind(this));
+        this.triggers = {};
 
-    public static instantiateInputSystem(map: InputMap) {
-        if (!InputSystem.instance) {
-            InputSystem.instance = new InputSystem(map);
+        for (let k in map) {
+            this.triggers[k] = map[k];
         }
     }
 
-    public static addButtonListener(eventType: EventType, key: string, onTriggered: () => void) {
-        InputSystem.instance.registerButtonListener(key, onTriggered, eventType);
+    public static initInputSystem(map: InputTriggerMap): void {
+        InputSystem.instance = new InputSystem(map);
     }
 
-    private registerButtonListener(key: string, onTriggered: () => void, eventType: EventType) {
-        var buttonArray: InputTriggerMap;
-
-        switch (eventType) {
-            case EventType.ButtonDown:
-                buttonArray = this.buttonDownTriggers;
-                break;
-            case EventType.ButtonUp:
-                buttonArray = this.buttonUpTriggers;
-                break;
-        }
-
-        const code = this.maps[key];
-        if (!code) {
-            console.warn(`Failed to find map for key: ${key}`);
-            return;
-        }
-
-        if (!buttonArray[code]) {
-            buttonArray = {...buttonArray, [code]: []}
-        }
-
-        let triggers = buttonArray[code];
-        triggers.push(onTriggered);
-    }
-
-    private onButtonDown(e: any) {
-        for (const [code, triggers] of Object.entries(this.buttonDownTriggers)) {
-            if (e.code === code || e.keyCode === code) {
-                triggers.forEach(trigger => trigger());
-                break;
+    private buttonPressed(e: any) : void {
+        for (let k in this.triggers) {
+            if (k === e.code) {
+                this.triggers[k]();
+                return;
             }
-        }
-    }
 
-    private onButtonUp(e: any) {
-        for (const [code, triggers] of Object.entries(this.buttonUpTriggers)) {
-            if (e.code === code || e.keyCode === code) {
-                triggers.forEach(trigger => trigger());
-                break;
-            }
         }
+
+        console.log(e.code + " is not bound to anything.");
     }
 }
 
-export type {InputMap, InputTriggerMap};
-export {InputSystem};
+export { InputSystem };
+export type { InputTriggerMap };
