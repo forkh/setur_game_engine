71cb44a77b039e3cf101dc089823301f7a4a279f
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-12 15:31:51 +0000

    Clean up

--- megatron/src/engine/GameObject.ts
+++ megatron/src/engine/GameObject.ts
@@ -136,12 +136,10 @@ class SpriteComponent extends GameComponent {
 class BoxColliderComponent extends GameComponent {
     public constructor(go: GameObject) {
         super(go);
-        //this.parent.getTransform().getPosition();
     }
 }
 
 type ControlMap = {
-    //[key: string]: (() => void);
     [key: string]: ((go: GameObject) => void);
 }
 
@@ -171,57 +169,6 @@ class ControllerComponent extends GameComponent {
             }
         }
     }
-
-    //public constructor(go: GameObject, inputs: ControlMap) {
-    //    super(go);
-    //    //document.addEventListener(, this.buttonPressed.bind(this));
-    //    this.triggers = inputs;
-
-    //    for (let k in inputs) {
-    //        document.addEventListener(k, inputs[k]);
-    //        this.triggers[k] = inputs[k];
-    //    }
-    //}
-
-    //private buttonPressed(e: any) : void {
-    //    for (let k in this.triggers) {
-    //        if (k === e.code) {
-    //            this.triggers[k](this.parent);
-    //            return;
-    //        }
-
-    //    }
-
-    //    console.log(e.code + " is not bound to anything.");
-    //}
-    //private controls: ControlMap;
-    ////private input: InputSystem;
-    //public constructor(controls: ControlMap, go: GameObject) {
-    //    console.log("New ControllerComponent");
-    //    super(go);
-    //    this.controls = {};
-    //    for (let controlsKey in controls) {
-    //        //document.addEventListener(controlsKey, this.controls[controlsKey]);
-    //        this.addListener(controlsKey, this.controls[controlsKey]);
-    //    }
-    //}
-
-    //public addListener(event: string, func: (go: GameObject) => void) {
-    //    document.addEventListener(event, this.doSomething.bind(this));
-    //    this.controls[event] = func;
-    //    //document.addEventListener(event, func);
-    //    console.log("Registering listener '" + event + "' to execute function: " + func);
-    //}
-
-    //private doSomething(e: any): void {
-    //    for (let k in this.controls) {
-    //        if (k === e.type) {
-    //            console.log(e.type);
-    //            this.controls[k](this.parent);
-    //            return;
-    //        }
-    //    }
-    //}
 }
 
 export { GameComponent, GameObject , ControllerComponent };
--- megatron/src/flagsifuglur.tsx
+++ megatron/src/flagsifuglur.tsx
@@ -7,7 +7,6 @@ import GameLoop from './engine/GameLoop';
 
 import assets from './assets.json';
 import soundMappings from './soundMappings.json';
-//import inputMappings from './inputMappings.json';
 
 const inputMappings: InputTriggerMap = {
     'KeyT': f1,
@@ -17,9 +16,6 @@ const inputMappings: InputTriggerMap = {
     'KeyD': right,
     'KeyH': sideways,
     'KeyR': () => {document.dispatchEvent(new Event("random"))}
-
-//    'KeyF': f2,
-//    'KeyR': f3
 }
 function sideways() {
     document.dispatchEvent(new Event("move_sideways"));
@@ -46,19 +42,9 @@ function f1() {
     console.log("Some shound should be heard!");
 }
 
-
-//function f2() {
-//    //obj2.getTransform().translate(3, 3);
-//    //obj1.getTransform().setPosition(10, 10);
-//    //console.log(obj1.getTransform().getPosition());
-//    console.log(obj1.getObjectID() + ": (" + obj1.getTransform().getPosition().getY() + ", " + obj1.getTransform().getPosition().getY() + ")");
-//}
-
-//function f3() {
-//    obj1.getTransform().translate(3, 3);
-//}
-
 const engine: Engine = new Engine(inputMappings, soundMappings, assets);
+
+// Extend GameComponent for specialization
 class BirdComponent extends GameComponent {
     //private go: GameObject;
     parent: GameObject;
@@ -75,13 +61,8 @@ class BirdComponent extends GameComponent {
     }
 
     private squeek(): void {
-        //console.log("squeek!");
-        //console.log(this.parent);
-        //this.parent.getTransform().translate(3, 0);
-        //this.go.getTransform().getPosition();
-        //this.parent.getTransform();
-        //const go: GameObject = super.getParent();
-        //go.getTransform().translate(3, 0);
+        console.log("squeek!");
+        this.parent.getTransform().translate(3, 0);
     }
 
 
@@ -114,21 +95,6 @@ engine.addTrack("s1", 1);
 engine.addTrack("s2", 2);
 engine.addTrack("s3", 3);
 engine.addTrack("s4", 4);
-
-
-//let obj1: GameObject = new GameObject(1);
-//obj1.addSprite("bird");
-//engine.addGameObject(obj1);
-
-//const obj2: GameObject = new GameObject(1);
-//obj2.addSprite("background");
-//engine.addGameObject(obj2);
-
-
-//const gc1: GameComponent = createComponent(25, 25, engine.getImage("bird").src);
-//engine.addGameComponent(gc1);
-//const bg: GameComponent = createComponent(0, 0, engine.getImage("background").src);
-//engine.addGameComponent(bg);
 
 function FlagsiFuglur() {
     let f = GameLoop();
