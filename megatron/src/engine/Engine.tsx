029babafb5c8976569ec899e4bfb16d885c8fdec
Runi Klein Hansen <runi.klein.hansen.fo@gmail.com>
2022-03-19 20:21:24 +0000

    Removed outcommented code

--- megatron/src/engine/Engine.tsx
+++ megatron/src/engine/Engine.tsx
@@ -4,8 +4,6 @@ import { InputSystem, InputTriggerMap } from './InputSystem';
 import { ReactNode } from 'react';
 import Canvas from './Canvas';
 import { CanvasProps } from './Canvas';
-//import { GameComponent, createComponent } from './GameComponent';
-//import { GameObject, GameComponent, ControlMap } from './GameObject';
 import { GameObject, GameComponent, ControlMap, ControllerComponent } from './GameObject';
 import { CollisionSystem, CollisionProps } from './CollisionSystem';
 import PhysicsSystem from './PhysicsSystem';
@@ -14,15 +12,10 @@ import GameLoop from './GameLoop';
 import { Vector2d } from './math';
 
 class Engine {
-    //private static instance: Engine;
-    private gameObjects: GameObject[] = [];// = [new GameObject(-1)];
+    private gameObjects: GameObject[] = [];
     private collisionObjects: GameObject[] = [];
-    //private gameObjects: GameObject[] = new Array<GameObject>();
     private static debug: boolean = true;
     public static base_case: number;
-    //public static scale: number;
-    //public static aspectRatio: number;
-    //public static proportion: number;
     private width: number;
     private height: number;
 
@@ -46,50 +39,17 @@ class Engine {
     public constructor(inputMap: InputTriggerMap, soundMapping: EventSounds, assets: AssetsType, width: number, height: number) {//, base_case: number, aspect_ratio: number, proportion: number) {
         this.height = height;
         this.width = width;
-    //public constructor(inputMap: InputTriggerMap, soundMapping: EventSounds, assets: AssetsType, base_case: number, aspect_ratio: number, proportion: number) {
-        //Engine.base_case = base_case;
-        //Engine.scale = window.innerHeight / Engine.base_case;
-        //Engine.aspectRatio = aspect_ratio;
-        //Engine.proportion = proportion;
-        //document.addEventListener("resize", () => {
-        //    Engine.scale = window.innerHeight / Engine.base_case;
-        //})
+
         document.addEventListener("toggle_debug", () => {
             Engine.debug = !Engine.debug;
         })
-        //this.gameObjects = [];
+
         ResourceManager.instantiateResourceManager(assets);
         AudioSystem.instantiateAudioSystem(soundMapping);
         InputSystem.instantiateInputSystem(inputMap);
         PhysicsSystem.instantiatePhysicsSystem(this.gameObjects);
         PhysicsSystem.start();
-        //setInterval()
-        //const collisionProp: CollisionProps = {
-        //    gameObjects: this.gameObjects
-        //}
-        //setInterval(this.checkForCollisions, 2000);
-        //CollisionSystem.getInstance(collisionProp);
-        //CollisionSystem.start();
-        // TODO: CollisionSystem:
-
-        // TODO: PhysicsSystem:
     }
-
-    //private test() {
-    //    this.gameObjects.forEach((go1) => {
-    //        this.gameObjects.forEach((go2) => {
-    //            if (go1.getObjectID() != go2.getObjectID()
-    //            //go1.hasCollider() && go2.hasCollider()) {
-    //                //collision logic
-    //            }
-    //        })
-    //    })
-    //}
-
-    // TODO: SLETTA!
-    //public getObjects(): GameObject[] {
-    //    return this.gameObjects;
-    //}
 
     public getImage(image: string): HTMLImageElement {
         return ResourceManager.getImage(image, false) as HTMLImageElement;
@@ -108,8 +68,6 @@ class Engine {
             width: this.width//window.innerHeight// * Engine.proportion * Engine.aspectRatio,//0.5625,
             //scale: Engine.scale
         }
-       // return <Canvas gameComponents={this.gameComponents} height={600} width={100}/>;
-        //return <Canvas objects={this.gameObjects} height={600} width={100}/>
         return <div><Canvas {...canvasProps}/>{gameLoop}</div>
     }
 
@@ -140,10 +98,6 @@ class Engine {
     }
 
     private checkForCollisions(): void {
-        //console.log("Checking for collisions");
-        //const scale: number = window.innerWidth / Engine.base_case;
-        //this.gameObjects.forEach((go1: GameObject) => {
-        //    this.gameObjects.forEach((go2: GameObject) => {
         if (this.gameObjects.length < 2) {
             return;
         }
@@ -156,23 +110,7 @@ class Engine {
             let go1: GameObject = this.collisionObjects[i];
             for (let j = 0; j < this.gameObjects.length; j++) {
                 let go2: GameObject = this.gameObjects[j];
-                //console.log("321oyoyoy");
-                //if (go1.hasBoxCollider() && go2.hasBoxCollider()) {
                 if (go1.hasBoxCollider() && go2.hasBoxCollider() && go1.getObjectID() != go2.getObjectID()) {
-                    //console.log("yoyoyo123");
-                    //// @ts-ignore
-                    //const w1: number = go1.getBoxCollider().width;
-                    //// @ts-ignore
-                    //const h1: number = go1.getBoxCollider().height;
-                    //const x1: number = go1.getTransform().getPosition().getX() - w1 / 2;
-                    //const y1: number = go1.getTransform().getPosition().getY() - h1 / 2;
-                    //// @ts-ignore
-                    //const w2: number = go2.getBoxCollider().width;
-                    //// @ts-ignore
-                    //const h2: number = go2.getBoxCollider().height;
-                    //const x2: number = go2.getTransform().getPosition().getX() - w2 / 2;
-                    //const y2: number = go2.getTransform().getPosition().getY() - h2 / 2;
-                    // @ts-ignore
                     const w1: number = go1.getBoxCollider().width;
                     // @ts-ignore
                     const h1: number = go1.getBoxCollider().height;
@@ -185,30 +123,6 @@ class Engine {
                     const x2: number = (go2.getTransform().getPosition().getX());
                     const y2: number = (go2.getTransform().getPosition().getY());
 
-                    //const w1: number = go1.getBoxCollider().width;
-                    //// @ts-ignore
-                    //const h1: number = go1.getBoxCollider().height;
-                    //const x1: number = (go1.getTransform().getPosition().getX() - w1);
-                    //const y1: number = (go1.getTransform().getPosition().getY() - h1);
-                    //// @ts-ignore
-                    //const w2: number = go2.getBoxCollider().width;
-                    //// @ts-ignore
-                    //const h2: number = go2.getBoxCollider().height;
-                    //const x2: number = (go2.getTransform().getPosition().getX() - w2);
-                    //const y2: number = (go2.getTransform().getPosition().getY() - h2);
-                    //if (x1 < (x2 + h2) &&
-                    //    (x1 + w1) > x2 &&
-                    //    y1 < (y2 + h2) &&
-                    //    (y1 + h1) > h2) {
-                    //    //return true;
-                    //    //send signal
-                    //    console.log("=============Collision============");
-                    //    console.log(`${go1.getObjectID()} and ${go2.getObjectID()}`);
-                    //    document.dispatchEvent(new Event("collision"));
-
-                    //}
-                    //console.log(`${go1.getObjectID()}: x: ${x1}, y: ${y1}, w: ${w1}, h: ${h1}, isSolid: ${go1.solid}`);
-                    //console.log(`${go2.getObjectID()}: x: ${x2}, y: ${y2}, w: ${w2}, h: ${h2}, isSolid: ${go2.solid}`);
                     if (
                         x1 + w1 / 2 > x2 - w2 / 2 &&
                         x1 - w1 / 2 < x2 + w2 / 2 &&
@@ -238,11 +152,7 @@ class Engine {
 
 
 
-                        //return true;
-                        //send signal
-                        console.log("=============Collision============");
-                        //document.dispatchEvent(new Event("collision"));
-                        //console.log("[[[[[[[[[]]]]]]]]]]]]]]");
+                        //console.log("=============Collision============");
                         document.dispatchEvent(new CustomEvent("collision2", {detail: {'obj': go1, 'obj2': go2}}));
 
 
@@ -281,6 +191,5 @@ function compareZIndex(go1: GameObject, go2: GameObject): number {
     return 0;
 }
 
-//export { Engine, GameObject, GameComponent };//, ControllerComponent };
 export { Engine, GameObject, GameComponent, ControllerComponent, Vector2d };
 export type { InputTriggerMap, AssetsType, ControlMap };
